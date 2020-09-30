7954814f4bbd737fc420d91aa86ae36fbf70c8c6
Shravan Srinivas <shravansrinivasgs56@gmail.com>
2020-09-30 22:37:56 +0530

    header portrait responsiveness

--- react-client/src/components/header/header.js
+++ react-client/src/components/header/header.js
@@ -3,14 +3,14 @@ import React from "react";
 function Header() {
   return (
     <header className="header-style">
-      <h3 style={{ textAlign: "left" }}>
+      <p style={{ textAlign: "left" }}>
         <img src="/images/tic-tac-toe.png" alt=""></img>
         React TTT Game
         <p style={{ float: "right" }}>
           {"\t|\t"}
           {/*  eslint-disable-next-line */}
           <a target="_blank" href="https://github.com/shravansrinivas/ttt">
-            View Source Code
+            View Code
           </a>
           {"\t|\t"} 
           {/*  eslint-disable-next-line */}
@@ -18,10 +18,10 @@ function Header() {
             target="_blank"
             href="https://github.com/shravansrinivas/ttt#readme"
           >
-            View Documentation
+            View Docs
           </a>
         </p>
-      </h3>
+      </p>
     </header>
   );
 }
